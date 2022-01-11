bceb2f0288396c6498d16dc3d68cb582f2eaab0f
imcat <shinko168@gmail.com>
2022-01-11 14:00:04 +0800

    update

--- jd_super_box.js
+++ jd_super_box.js
@@ -27,7 +27,7 @@ let cookiesArr = [],
     cookie = '',
     secretp = '',
     joyToken = "";
-$.shareCoseList = ['Xx33Brrod56EjLp84YQz1w','jEEqauh3scwZiswwKd_47os-FTBxr-oceiCIf7F6ey8'];
+$.shareCoseList = ['Xx33Brrod56EjLp84YQz1w','jEEqauh3scwZiswwKd_47os-FTBxr-oceiCIf7F6ey8','_8RB2wZ1LzboqWhnmkWfNg','zMpLgDvo8MLJ81eQiEofwAGPZoXvaeojLDACG7QgVwY','YWI2U3eaeQXw7-RPX9BAbkcusJAcHAuCx_dui0d-icE','mdlCc2HrPThCqEwKK9KaAw','CRiEoqipvVz6WolZJzYeAw','kjQp4Dq1fW-1WdlY36kbpw','WrUHXi5Ytp62qj3f79RMaQ','MkeeuLVVmy7BIYj98Jo9iw'];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
         cookiesArr.push(jdCookieNode[item])
