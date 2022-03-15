8398fd89526c7d112dc906074f8da16288a24f7f
imcat <shinko168@gmail.com>
2022-03-15 09:42:28 +0800

    update

--- jd_price.js
+++ jd_price.js
@@ -85,7 +85,7 @@ let h5st = ''
          if (i != cookiesArr.length - 1) {
             await jstoken();
          }
-         await $.wait(60 * 1000)
+         await $.wait(80 * 1000)
       }
    }
    if (allMessage) {
