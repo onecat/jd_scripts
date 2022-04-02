0e1bad9276a5a9d7d5f509c1df3accd67dff31fa
imcat <shinko168@gmail.com>
2022-04-02 12:05:27 +0800

    update

--- jd_cash.js
+++ jd_cash.js
@@ -440,6 +440,7 @@ function getSign(functionId, body) {
 		        'Accept': '*/*',
 		        "accept-encoding": "gzip, deflate, br",
 		        'Content-Type': 'application/json',
+				"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTEyMDY4ODYxLCJpYXQiOjE2NDg4NzA4NTgsImV4cCI6MTY4MDQwNjg1OH0.8sF5gMJYU6Qyppjk2E5je2yf1M7mCsmun2LXlYcIh4E"
 		    },
 		    timeout: 30000
         }
